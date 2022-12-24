27486aeafb674b826a518c8ea8c718d714966d40
PeterHolmqvist <peterholmqvist@hotmail.com>
2022-12-24 07:37:29 +0000

    cleaned up some code in settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fr*z4(9phen*bcakf#(zt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
