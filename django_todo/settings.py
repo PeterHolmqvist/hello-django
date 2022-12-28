3ea0ba5e0ceb21754b8b45e958abc4505fa2e686
PeterHolmqvist <peterholmqvist@hotmail.com>
2022-12-28 12:39:46 +0000

    added secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = development
 
 if development:
     ALLOWED_HOSTS = ['localhost']
@@ -89,7 +89,7 @@ if development:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 else:
